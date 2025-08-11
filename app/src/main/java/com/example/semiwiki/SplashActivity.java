72877a3b9d2c46027c4150014ecf191d5c23794c
ashxom <anseha090115@naver.com>
2025-08-11 14:47:50 +0900

    feat: 스플래시 화면 추가

---
+++ app/src/main/java/com/example/semiwiki/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.semiwiki;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 500); // 0.5초 후 이동
+    }
+}
