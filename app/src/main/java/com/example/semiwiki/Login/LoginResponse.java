ff9d5b258be1d024c89cc5f01a6f5ebb75e651f3
ashxom <anseha090115@naver.com>
2025-09-16 13:26:47 +0900

    fix: 로그인 토큰 적용 & 리스트 자동 갱신 (#52)
    + 검색 아이콘 삭제

--- app/src/main/java/com/example/semiwiki/Login/LoginResponse.java
+++ app/src/main/java/com/example/semiwiki/Login/LoginResponse.java
@@ -2,14 +2,12 @@ package com.example.semiwiki.Login;
 
 import com.google.gson.annotations.SerializedName;
 
-// 서버에서 로그인 성공 시 응답으로 내려주는 JSON을 담는 클래스
 public class LoginResponse {
 
-    // 응답 JSON: { "access_token": "...", "refresh_token": "..." }
-    @SerializedName("access_token")
+    @SerializedName(value = "accessToken",  alternate = {"access_token"})
     private String accessToken;
 
-    @SerializedName("refresh_token")
+    @SerializedName(value = "refreshToken", alternate = {"refresh_token"})
     private String refreshToken;
 
     @SerializedName(value = "accountId", alternate = {"account_id", "userId"})
